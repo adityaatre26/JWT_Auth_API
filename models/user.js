77ce01e34331a9872b31380bdd0c4f87f2c2a0f9
adityaatre26 <148355615+adityaatre26@users.noreply.github.com>
2024-10-13 19:04:34 +0530

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model("User", userSchema);
